e030400cd3652e7dcaf60e7e5c746c4fd641fafa
Issei.Muratani <muurann@gmail.com>
2016-04-20 08:43:29 +0000

    work 20160420

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :collect_user, only: [:edit, :update]
+  
   def show # 追加
     @user = User.find(params[:id])
   end
@@ -18,10 +20,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit # 追加
+    @user = User.find(params[:id])
+  end
+
+  def update # 追加
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Updated"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end 
+  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location)
+  end
+  
+  def collect_user
+    user = User.find(params[:id])
+    redirect_to(root_url) if user != current_user
   end
 end
